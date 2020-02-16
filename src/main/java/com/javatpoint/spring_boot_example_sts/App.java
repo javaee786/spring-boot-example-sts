61936e1bc8647e5dfc3502c114d253ead1443508
Raghunadh <Raghunadh@RAGHU>
2020-02-16 22:08:48 +0530

    1st commit

---
+++ src/main/java/com/javatpoint/spring_boot_example_sts/App.java
@@ -0,0 +1,11 @@
+package com.javatpoint.spring_boot_example_sts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
